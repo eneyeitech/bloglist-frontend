dc65484db5301470cca1d9fea559e6d2a52a8652
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2025-09-16 13:30:59 +0100

    5.14: Blog List Tests, step 2 Implemented

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,7 +1,39 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders blog title and author, but not url or likes by default', () => {
+
+describe('<Blog />', () => {
+    test('renders blog title and author, but not url or likes by default', () => {
+        const blog = {
+            title: 'Testing React components',
+            author: 'John Doe',
+            url: 'http://example.com',
+            likes: 10,
+        }
+
+        render(<Blog blog={blog} />)
+
+        // Title and author are shown
+        const title = screen.getByText('Testing React components')
+        const author = screen.getByText('John Doe')
+        expect(title).toBeInTheDocument()
+        expect(author).toBeInTheDocument()
+
+        const url = screen.queryByText('http://example.com')
+        const likes = screen.queryByText('likes 10')
+
+        // Url and likes are NOT shown
+        expect(url).toBeNull()
+        expect(likes).toBeNull()
+
+        const details = screen.queryByRole('region', { name: 'blog-details' })
+            || screen.queryByText(blog.url)
+
+        expect(details).toBeNull()
+    })
+
+    test('shows url and likes when the view button is clicked', async () => {
   const blog = {
     title: 'Testing React components',
     author: 'John Doe',
@@ -9,23 +41,18 @@ test('renders blog title and author, but not url or likes by default', () => {
     likes: 10,
   }
 
-  render(<Blog blog={blog} />)
+  const userObj = {
+    username: 'eneye'
+  }
 
-  // Title and author are shown
-  const title = screen.getByText('Testing React components')
-  const author = screen.getByText('John Doe')
-  expect(title).toBeInTheDocument()
-  expect(author).toBeInTheDocument()
+  render(<Blog blog={blog} user={userObj} />)
 
-  const url = screen.queryByText('http://example.com')
-  const likes = screen.queryByText('likes 10')
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
 
-  // Url and likes are NOT shown
-  expect(url).toBeNull()
-  expect(likes).toBeNull()
-
-  const details = screen.queryByRole('region', { name: 'blog-details' }) 
-    || screen.queryByText(blog.url) 
-
-  expect(details).toBeNull()
+  // url and likes should now appear
+  expect(screen.getByText(blog.url)).toBeInTheDocument()
+  expect(screen.getByText(`likes ${blog.likes}`)).toBeInTheDocument()
+})
 })
