75f456a708fa395225f1bb85d123954420bc9f1c
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2025-09-29 20:43:34 +0100

    7.20: Styles, step 1 Implemented

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,6 +2,48 @@ import { useDispatch } from 'react-redux'
 import { likeBlog, removeBlogFromStore } from '../reducers/blogReducer'
 import { showNotification } from '../reducers/notificationReducer'
 import { useState } from 'react'
+import styled from 'styled-components'
+
+// Styled components
+const BlogContainer = styled.div`
+  background: #fff;
+  border: 1px solid #ccc;
+  border-radius: 6px;
+  padding: 0.75rem;
+  margin-bottom: 0.75rem;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
+`
+
+const BlogHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const Title = styled.span`
+  font-weight: bold;
+  color: #333;
+`
+
+const Button = styled.button`
+  background: #1976d2;
+  color: white;
+  border: none;
+  padding: 0.3rem 0.6rem;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-left: 0.5rem;
+
+  &:hover {
+    background: #1565c0;
+  }
+`
+
+const Details = styled.div`
+  margin-top: 0.5rem;
+  padding-left: 0.5rem;
+  color: #444;
+`
 
 const Blog = ({ blog, user }) => {
   const dispatch = useDispatch()
@@ -17,46 +59,37 @@ const Blog = ({ blog, user }) => {
   }
 
   const handleDelete = () => {
-  if (window.confirm(`Remove blog ${blog.title} by ${blog.author}?`)) {
-    dispatch(removeBlogFromStore(blog.id))
-    dispatch(showNotification(`Blog '${blog.title}' deleted`, 5))
-  }
-}
-
-
-  const blogStyle = {
-    paddingTop: 10,
-    paddingLeft: 2,
-    border: 'solid',
-    borderWidth: 1,
-    marginBottom: 5,
+    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}?`)) {
+      dispatch(removeBlogFromStore(blog.id))
+      dispatch(showNotification(`Blog '${blog.title}' deleted`, 5))
+    }
   }
 
   return (
-    <div style={blogStyle} className="blog">
-      <div>
-        {blog.title} {blog.author}{' '}
-        <button onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</button>
-      </div>
+    <BlogContainer>
+      <BlogHeader>
+        <Title>
+          {blog.title} {blog.author}
+        </Title>
+        <Button onClick={toggleVisibility}>
+          {visible ? 'hide' : 'view'}
+        </Button>
+      </BlogHeader>
 
       {visible && (
-        <div className="blogDetails">
+        <Details>
           <div>{blog.url}</div>
           <div>
             likes {blog.likes}{' '}
-            <button onClick={handleLike} className="likeButton">
-              like
-            </button>
+            <Button onClick={handleLike}>like</Button>
           </div>
           <div>{blog.user?.name}</div>
           {user.username === blog.user?.username && (
-            <button onClick={handleDelete} className="removeButton">
-              remove
-            </button>
+            <Button onClick={handleDelete}>remove</Button>
           )}
-        </div>
+        </Details>
       )}
-    </div>
+    </BlogContainer>
   )
 }
 
